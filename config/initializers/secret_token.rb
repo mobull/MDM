3d5e97dbb10e90667c12f0397a4fedaabb537639
Marco Yin <xmarco.info@gmail.com>
2012-12-05 11:32:23 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MDM::Application.config.secret_token = 'ccdc5aa4048f611aa156b26211cde8ab62d72970aa7fb16320f64668bcb1966cdadec353d72298bcadbe9748ff408b95facb74e064d91135581920cbc4c6bb96'

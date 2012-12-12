f1d623060e48ceb6636fd46be8d4c6f57b53d849
Marco Yin <xmarco.info@gmail.com>
2012-12-13 01:51:32 +0800

    prevent the creation of platform records when platform table has been created

--- config/initializers/platforms.rb
+++ config/initializers/platforms.rb
@@ -1,3 +1,5 @@
-Platform::IDENTIFIER_DISPLAY_NAME_MAP.each do |identifier, display_name|
-  Platform.send(identifier)
+if Platform.table_exists?
+  Platform::IDENTIFIER_DISPLAY_NAME_MAP.each do |identifier, display_name|
+    Platform.send(identifier)
+  end
 end

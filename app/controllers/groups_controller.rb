855b05e0fe9b8fb7063eadae9fa94652abcd9895
Vincent Wan <wellber.wan@gmail.com>
2012-12-20 02:14:53 +0800

    fix index page and create new/show/edit pages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,4 +8,16 @@ class GroupsController < ApplicationController
   def index 
     @groups = Group.all
   end
+
+  # def show 
+  #   @group = Group.find(params[:id])
+  # end
+
+  def new 
+    @group = Group.new
+  end
+
+  def edit 
+    @group = Group.find(params[:id])
+  end
 end

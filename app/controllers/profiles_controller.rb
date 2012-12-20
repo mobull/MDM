c454ae3a95b8cb67031ff5867f26f6ac6fcb227f
Vincent Wan <wellber.wan@gmail.com>
2012-12-21 02:15:33 +0800

    modify profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,7 @@
-class ProfileController < ApplicationController
+class ProfilesController < ApplicationController
+	layout 'admin'
+
+  before_filter :authenticate_user!
+
+  respond_to :html, :json
 end
